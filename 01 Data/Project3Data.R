9adee0e1bdb4a8da3778e9500d3e44ddf8cd28ca
Adam Humphrey <adamthumph@gmail.com>
2015-03-03 16:00:47 -0600

    More work on loading in data frames (still not 100% fixed)

--- 01 Data/Project3Data.R
+++ 01 Data/Project3Data.R
@@ -1,7 +1,7 @@
 #This is the placeholder for the 01 Data folder
 
 require("jsonlite")
-require("rjson")
+require("RCurl")
 
 leather <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from ArticleLeather"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_das3734', PASS='orcl_das3734', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
