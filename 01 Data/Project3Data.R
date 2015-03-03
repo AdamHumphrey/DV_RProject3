ab9cc4f1baa458a6c4a13101b966fa8e94cc9b79
Adam Humphrey <adamthumph@gmail.com>
2015-03-03 14:37:43 -0600

    Pulling data frames into R Studio (may be broken, beware)

--- 01 Data/Project3Data.R
+++ 01 Data/Project3Data.R
@@ -1,1 +1,10 @@
 #This is the placeholder for the 01 Data folder
+
+require("jsonlite")
+require("rjson")
+
+leather <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from ArticleLeather"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_das3734', PASS='orcl_das3734', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+aircraft <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from aircraft"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_das3734', PASS='orcl_das3734', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+ammo <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from ammo"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_das3734', PASS='orcl_das3734', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
