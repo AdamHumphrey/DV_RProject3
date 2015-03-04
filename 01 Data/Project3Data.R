aa5ad35f2f5de3e75c3f3ad3b18b2b04d5769017
Adam Humphrey <adamthumph@gmail.com>
2015-03-04 17:33:24 -0600

    Working changes

--- 01 Data/Project3Data.R
+++ 01 Data/Project3Data.R
@@ -1,14 +1,14 @@
-#This is the placeholder for the 01 Data folder
-
 require("jsonlite")
 require("RCurl")
 
-
-
-
-
 leather <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from articleleather_revised"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_das3734', PASS='orcl_das3734', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+head(leather)
 
 aircraft <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from aircraft_revised"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_das3734', PASS='orcl_das3734', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
+head(aircraft)
+
 ammo <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from ammo_revised"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_das3734', PASS='orcl_das3734', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+head(ammo)
