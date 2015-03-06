098d96591ba4e547da2a16e1153bd280fb2512aa
Adam Humphrey <adamthumph@gmail.com>
2015-03-06 14:23:26 -0600

    Visualization changes

--- 02 Data Wrangling/Project3DataWrangling.R
+++ 02 Data Wrangling/Project3DataWrangling.R
@@ -14,11 +14,11 @@ tbl_df(G2)
 R1 <- aircraft %>% select(AIRCRAFT_YEAR,AIRCRAFT_COUNTRY_AREA) %>% distinct() %>% mutate(YEAR=as.character(AIRCRAFT_YEAR)) %>% mutate(COUNTRY=as.character(AIRCRAFT_COUNTRY_AREA)) %>% select(YEAR,COUNTRY); R2 <- ammo %>% select(AMMO_YEAR,AMMO_COUNTRY_AREA) %>% distinct() %>% mutate(YEAR=as.character(AMMO_YEAR)) %>% mutate(COUNTRY=as.character(AMMO_COUNTRY_AREA)) %>% select(YEAR,COUNTRY); R3 <- leather %>% select(AL_YEAR,AL_COUNTRY_AREA) %>% distinct() %>% mutate(YEAR=as.character(AL_YEAR)) %>% mutate(COUNTRY=as.character(AL_COUNTRY_AREA)) %>% select(YEAR,COUNTRY); G3 <- intersect(R1,R2) %>% intersect(.,R3) %>% arrange(YEAR) %>% arrange(COUNTRY)
 tbl_df(G3)
 
-aircrafttrades <- G2 %>% select(NUMBER_DATAPOINTS, AIRCRAFT_COUNTRY_AREA) %>% filter(!is.na(AIRCRAFT_COUNTRY_AREA))
+aircrafttrades <- G2 %>% select(NUMBER_DATAPOINTS, AIRCRAFT_COUNTRY_AREA) %>% filter(!is.na(AIRCRAFT_COUNTRY_AREA)) %>% slice(206:215)
 tbl_df(aircrafttrades)
 
-ammotrades <- G2 %>% select(NUMBER_DATAPOINTS, AMMO_COUNTRY_AREA) %>% filter(!is.na(AMMO_COUNTRY_AREA))
+ammotrades <- G2 %>% select(NUMBER_DATAPOINTS, AMMO_COUNTRY_AREA) %>% filter(!is.na(AMMO_COUNTRY_AREA)) %>% slice(198:207)
 tbl_df(ammotrades)
 
-altrades <- G2 %>% select(NUMBER_DATAPOINTS, AL_COUNTRY_AREA) %>% filter(!is.na(AL_COUNTRY_AREA))
+altrades <- G2 %>% select(NUMBER_DATAPOINTS, AL_COUNTRY_AREA) %>% filter(!is.na(AL_COUNTRY_AREA)) %>% slice(203:212)
 tbl_df(altrades)
--- 03 Visualizations/Project3Visualizations.R
+++ 03 Visualizations/Project3Visualizations.R
@@ -5,11 +5,11 @@ require("gplots")
 
 ggplot(G1, aes(x = AIRCRAFT_COUNTRY_AREA, y = NUMBER_DATAPOINTS)) + geom_point(colour = "dodgerblue2") + ggtitle("Same Number of Trades")
 
-tradeplot1 <- ggplot(aircrafttrades, aes(x = NUMBER_DATAPOINTS, y = AIRCRAFT_COUNTRY_AREA)) + geom_point(colour="dodgerblue2") + labs(x = "Aircraft Trade Countries") + ggtitle("Aircraft Trades") + theme(plot.title=element_text(size=20, face="bold", vjust=2))
+tradeplot1 <- ggplot(aircrafttrades, aes(x = AIRCRAFT_COUNTRY_AREA, y = NUMBER_DATAPOINTS)) + geom_area(colour="dodgerblue2") + labs(x = "Aircraft Trade Countries", y = "Number of Trades") + ggtitle("Aircraft Trades") + theme(plot.title=element_text(size=20, face="bold", vjust=2))
 
-tradeplot2 <- ggplot(ammotrades, aes(x = NUMBER_DATAPOINTS, y = AMMO_COUNTRY_AREA)) + geom_point(colour="red") + labs(x = "Number of Ammo Trades", y = "Countries") + ggtitle("Ammo Trades") + theme(plot.title=element_text(size=20, face="bold", vjust=2))
+tradeplot2 <- ggplot(ammotrades, aes(x = AMMO_COUNTRY_AREA, y = NUMBER_DATAPOINTS)) + geom_area(colour="red") + labs(x = "Number of Ammo Trades", y = "Countries") + ggtitle("Ammo Trades") + theme(plot.title=element_text(size=20, face="bold", vjust=2))
 
-tradeplot3 <- ggplot(altrades, aes(x = NUMBER_DATAPOINTS, y = AL_COUNTRY_AREA)) + geom_point(colour="chocolate") + labs(x = "Number of Leather Trades", y = "Countries") + ggtitle("Article Leather Trades") + theme(plot.title=element_text(size=20, face="bold", vjust=2))
+tradeplot3 <- ggplot(altrades, aes(x = AL_COUNTRY_AREA, y = NUMBER_DATAPOINTS)) + geom_area(colour="chocolate") + labs(x = "Number of Leather Trades", y = "Countries") + ggtitle("Article Leather Trades") + theme(plot.title=element_text(size=20, face="bold", vjust=2))
 
 tradeplot1
 tradeplot2
